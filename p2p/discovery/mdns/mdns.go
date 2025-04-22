2dbcac776aeab7b7c83e626339fbaac5cc8855e8
xinyuliang <xinyu.liang@igg.com>
2025-04-22 10:40:21 +0800

    添加测试日志

--- p2p/discovery/mdns/mdns.go
+++ p2p/discovery/mdns/mdns.go
@@ -68,7 +68,7 @@ func NewMdnsService(host host.Host, serviceName string, notifee Notifee) *mdnsSe
 func (s *mdnsService) Start() error {
 	// 在 Android 平台上完全跳过启动 Mdns 服务以避免权限问题
 	if runtime.GOOS == "android" {
-		log.Info("在 Android 平台上跳过 mDNS 服务启动。")
+		log.Debug("mDNS: Detected Android, skipping service start.")
 		return nil // 阻止服务器和解析器启动
 	}
 
